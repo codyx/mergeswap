91877fd359c52eb8d707c4744a762ad677ad2d5c
liamzebedee <liamzebedee@yahoo.com.au>
2022-09-14 02:18:57 +1000

    commit failing code

--- contracts/test/WrappedPoWETH.test.ts
+++ contracts/test/WrappedPoWETH.test.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { Wallet, BigNumber } from "ethers";
-import { hexZeroPad, joinSignature, keccak256, parseEther } from "ethers/lib/utils";
+import { defaultAbiCoder, hexZeroPad, joinSignature, keccak256, parseEther } from "ethers/lib/utils";
 
 import { proof, stateRoot } from "./mocks/proof";
 import { encodeProof } from "./utils/encode-proof";
@@ -68,7 +68,11 @@ describe("ReceiveWPoW", function () {
 
     // 2. Deploy the Deposit contract on POW.
     const DepositPoW = await ethers.getContractFactory("DepositPoW", config.pow.signer);
-    const depositPoW = await DepositPoW.deploy(relayer.address, nextPOSContractAddress, 6);
+    const depositPoW = await DepositPoW.deploy(
+      relayer.address, 
+      nextPOSContractAddress, 
+      6
+    );
     await depositPoW.deployed();
     console.log(`DepositPoW: ${depositPoW.address}`)
 
@@ -102,25 +106,74 @@ describe("ReceiveWPoW", function () {
     // Compute storage key.
     let storageKey
     {
-      const paddedSlot = hexZeroPad("0x3", 32);
-      const paddedKey = hexZeroPad("0x0", 32);
-      const itemSlot = keccak256(paddedKey + paddedSlot.slice(2));
-      storageKey = itemSlot
-      // const storageAt = await user.provider?.getStorageAt(
-      //   depositPoW.address,
-      //   itemSlot
-      // );
+      // Encode the call that does deposits/withdrawals(index).
+      // 
+      const ABI = [
+        "function deposits(uint256) public returns (bytes32)"
+      ];
+      const iface = new ethers.utils.Interface(ABI);
+      const calldata = iface.encodeFunctionData("deposits", [0])
+      // console.log(`calldata`, calldata)
+
+      // Call eth_createAccessList to get the relevant storage root from the RPC node.
+      // 
+      const tx = {
+        // from: signerAddress,
+        to: depositPoW.address,
+        data: calldata
+      }
+
+      const accessList = await config.pow.provider.send(
+        'eth_createAccessList',
+        [tx, "latest"]
+      );
+
+      console.log(`accessList`, accessList)
+      console.log(`accessList.accessList[0].storageKeys`, accessList.accessList[0].storageKeys)
+
+      storageKey = accessList.accessList[0].storageKeys[0]
+
+      // const deposit = await depositPoW.functions.deposits('0')
+      // console.log('deposit', deposit)
+
+      // const paddedSlot = hexZeroPad("0x3", 32);
+      // const paddedKey = hexZeroPad("0x0", 32);
+      // const itemSlot = keccak256(paddedKey + paddedSlot.slice(2));
+      // storageKey = itemSlot
+
+      const storageAt = await config.pow.provider.getStorageAt(
+        depositPoW.address,
+        storageKey
+      );
+      const depositInfo = keccak256(
+        defaultAbiCoder.encode(
+          ["uint256", "address"],
+          [depositAmount, signerAddress]
+        )
+      );
+      console.log(`depositInfo`, depositInfo)
+      console.log(`storageAt  `, storageAt)
     }
 
     // console.log(storageKey)
-    const proof = await config.pow.provider.send("eth_getProof", [depositPoW.address, [storageKey], 'latest'])
-    console.log(proof)
+    const proof = await config.pow.provider.send(
+      "eth_getProof",
+      [depositPoW.address, [storageKey], 'latest']
+    )
+    const block = await config.pow.provider.getBlock('latest')
+    const rawBlock = await config.pow.provider.send(
+      'eth_getBlockByNumber',
+      [ethers.utils.hexValue(block.number), true]
+    );
+    const stateRoot = rawBlock.stateRoot
+    console.log(`stateRoot`, stateRoot)
+    console.log(`proof`, proof)
 
     // Return the data for testing.
     // 
     const user = config.pos.signer
     return {
-      user, relayer, wrappedPowETH, proof
+      user, relayer, wrappedPowETH, proof, stateRoot
     }
   }
 
@@ -197,11 +250,24 @@ describe("ReceiveWPoW", function () {
   })
 
   describe.only("Mint", () => {
+    it.only('decodes proof', () => {
+      let accountProof = [
+        // '0xfefffb8093cc196784da800c8776e7eb5290583960b3cbb4b3f5569caeff4ca7f23dba12807b511c8799e32a00808434087c343511dcd07849cbb5f45a4f275baab66133be80688bb6316e187ea201816f5c7f0242ecacf68128d41326fff57c4cbf6a9d4940800ed14e0075e9a2de54a751a472d458459ed8f0396cd0e1923b31877efc89a67b803327e09465d5d22c7ba306b7d7e0c158c884ab6093c054469c4c624ff7de87538009e5979c9e269d8f089c04c0b614c256d33ddafb1ea77bff9470bffa4a4763ba80fe7c84703a63d094d41c58e8327363082bd014b357eb331915965c39e526e815804b328395ae1d4eb603815784657b60214b8f47a5401366c9e659a2fc4a0b32a98058215d25da39760b4a5287bd0365cd0dcb7b9c6dc90be0014a8333d1ae208dc880de07bb013e262d89bc7c8a0941c3754044d5ccd27d06147a2c48b66f2b27641f80e633ac29fdd55d3eaec196bef62432d18241f321231e66c1f9d625ce445f5e5b80b5a9fe21052c52d994846407df68e518a8e966591de76d092ad782630e3324958028e90c83e36c286bd2b46f0080d9488868b6f4642e935d0e2aed7db8084d3987803c3462bdac43916b6fec048675ec0ad3283f2414beb12473c1fa72b46f8d618980fca6c2b7b2b728561d25ab14abffc5f2a14c55752a2c52157dfca69e6be28275',
+        // '0xfe420880713cee8e8e8355ddcd0e16e77e8f1269e3c41ae085477260ae0b21a84ddc0b5280f722c817b9d9160896a1705742797ac1b2352973086a50503036fbf7de2ebc5f802b3fd4013d1d7aeeb3864b45183528292093bb67b1db7b1edd452b23441f655f',
+        '0x3f30247092931d8c66bf63db8788c6b72a7a8fcc5f0c53a0424bd8390131805e3901f84c01880de0b6b3a7640000a04b1f342f677692ad675a5f26211726e72938819571f6385d20681ae6fc98e560a0bfc84ae83377270ba8eecc7b0a942c8d7062dc9c8565bd98e42c1e475e6daa68'
+      ]
+
+      console.log(
+        accountProof.map((part: string) => ethers.utils.RLP.decode(part))
+      )
+    })
+    
     it("Should mint ETHPOW", async () => {
-      const { user, relayer, wrappedPowETH, proof } = await setupTestnets()
+      const { user, relayer, wrappedPowETH, proof, stateRoot } = await setupTestnets()
       // const { user, relayer, wrappedPowETH } = await loadFixture(
       //   deployReceiveWPoWFixture
       // );
+
       const sigRaw = await relayer._signingKey().signDigest(stateRoot);
       const sig = joinSignature(sigRaw);
 
@@ -210,11 +276,20 @@ describe("ReceiveWPoW", function () {
         .connect(user)
         .relayStateRoot(blockNumber, stateRoot, sig);
 
-      const accountProofEncoded = encodeProof(proof.accountProof);
+      proof.accountProof.map((part: string) => ethers.utils.RLP.decode(part))
+
+      // const accountProofEncoded = proof.accountProof
+      const accountProofEncoded = encodeProof(proof.accountProof); // FAILS HERE
+      //      Error: invalid rlp: total length is larger than the data
+      // Fails on the first item in the proof?
+
+      return
+
       await wrappedPowETH.updateDepositContractStorageRoot(
         blockNumber,
         accountProofEncoded
       );
+
 
       const storageProofEncoded = encodeProof(proof.storageProof[0].proof);
       await wrappedPowETH.mint(
